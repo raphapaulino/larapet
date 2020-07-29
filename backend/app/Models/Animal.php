debf39a4cd82b2112a1b5a406fde33272da03ccf
Raphael Paulino <raphael.paulino.web@gmail.com>
2020-07-28 23:59:39 -0300

    - fillable fields added, sluggable method created

--- backend/app/Models/Animal.php
+++ backend/app/Models/Animal.php
@@ -2,10 +2,40 @@
 
 namespace App\Models;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 use YourAppRocks\EloquentUuid\Traits\HasUuid;
 
 class Animal extends Model
 {
-    use HasUuid;
+    use HasUuid, Sluggable;
+
+    protected $fillable = [
+        'company_id',
+        'photo',
+        'name',
+        'born_date',
+        'slug',
+        'specie',
+        'color',
+        'gender',
+        'size',
+        'coat',
+        'breed',
+        'age',
+        'age_months',
+        'age_group',
+        'is_neutered',
+        'is_active',
+    ];
+
+    // from slugglabe package
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'name'
+            ]
+        ];
+    }
 }
